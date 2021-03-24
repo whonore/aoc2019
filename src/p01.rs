6a5aedc8e9d0b3573cd77fb1eb9bc683d0c96108
whonore <wolfhonore@gmail.com>
2021-03-24 10:47:00 -0400

    Solve problem 1 part 2

--- src/p01.rs
+++ src/p01.rs
@@ -1,9 +1,23 @@
 fn calc_fuel(mass: u64) -> u64 {
-    mass / 3 - 2
+    (mass / 3).saturating_sub(2)
 }
 
-fn solve(masses: &[u64]) -> u64 {
-    masses.iter().map(|m| calc_fuel(*m)).sum()
+fn calc_fuel_all(mass: u64) -> u64 {
+    let mut tot = 0;
+    let mut mass = mass;
+    while mass > 0 {
+        mass = calc_fuel(mass);
+        tot += mass;
+    }
+    tot
+}
+
+fn part1(masses: &[u64]) -> u64 {
+    masses.iter().copied().map(calc_fuel).sum()
+}
+
+fn part2(masses: &[u64]) -> u64 {
+    masses.iter().copied().map(calc_fuel_all).sum()
 }
 
 pub fn run() -> Result<String, String> {
@@ -14,7 +28,7 @@ pub fn run() -> Result<String, String> {
         .collect::<Result<Vec<_>, _>>()
         .map_err(|_| "Bad input")?;
     let out1 = part1(&masses);
-    let out2 = part2(&[]);
+    let out2 = part2(&masses);
     Ok(format!("{} {}", out1, out2))
 }
 
@@ -28,5 +42,12 @@ mod tests {
         assert_eq!(calc_fuel(14), 2);
         assert_eq!(calc_fuel(1969), 654);
         assert_eq!(calc_fuel(100756), 33583);
+    }
+
+    #[test]
+    fn test02() {
+        assert_eq!(calc_fuel_all(14), 2);
+        assert_eq!(calc_fuel_all(1969), 966);
+        assert_eq!(calc_fuel_all(100756), 50346);
     }
 }
