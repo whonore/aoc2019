4c9400a59a263c5aab151129850670097f2ea51a
whonore <wolfhonore@gmail.com>
2021-03-31 14:04:08 -0400

    Solve problem 9 part 2

--- src/p09.rs
+++ src/p09.rs
@@ -8,14 +8,18 @@ fn part1(prog: &Intcode) -> Result<i64, String> {
         .and_then(|out| out.first().copied().ok_or_else(|| "No return value".into()))
 }
 
-fn part2() -> u64 {
-    0
+fn part2(prog: &Intcode) -> Result<i64, String> {
+    prog.exec()
+        .read_vec(&[2])
+        .write_to(vec![])
+        .run()
+        .and_then(|out| out.first().copied().ok_or_else(|| "No return value".into()))
 }
 
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/p09.txt");
     let prog = input.parse()?;
     let out1 = part1(&prog)?;
-    let out2 = part2();
+    let out2 = part2(&prog)?;
     Ok(format!("{} {}", out1, out2))
 }
