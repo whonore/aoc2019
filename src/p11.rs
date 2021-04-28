fc4716c6c30ce895319756f7ec065686041fa706
whonore <wolfhonore@gmail.com>
2021-04-28 14:43:43 -0400

    Solve problem 11 part 2

--- src/p11.rs
+++ src/p11.rs
@@ -28,16 +28,12 @@ enum Dir {
 }
 
 impl Dir {
-    fn turn(self, t: Turn) -> Self {
+    const fn turn(self, t: Turn) -> Self {
         match (self, t) {
-            (Self::Up, Turn::Left) => Self::Left,
-            (Self::Down, Turn::Left) => Self::Right,
-            (Self::Left, Turn::Left) => Self::Down,
-            (Self::Right, Turn::Left) => Self::Up,
-            (Self::Up, Turn::Right) => Self::Right,
-            (Self::Down, Turn::Right) => Self::Left,
-            (Self::Left, Turn::Right) => Self::Up,
-            (Self::Right, Turn::Right) => Self::Down,
+            (Self::Up, Turn::Left) | (Self::Down, Turn::Right) => Self::Left,
+            (Self::Down, Turn::Left) | (Self::Up, Turn::Right) => Self::Right,
+            (Self::Left, Turn::Left) | (Self::Right, Turn::Right) => Self::Down,
+            (Self::Right, Turn::Left) | (Self::Left, Turn::Right) => Self::Up,
         }
     }
 }
@@ -107,35 +103,67 @@ impl Robot {
             Dir::Right => self.x += 1,
         }
     }
+
+    fn run(&mut self, prog: &Intcode) -> Result<(), String> {
+        let mut prog = prog.exec().read_vec(&[]).write_to(vec![]);
+        loop {
+            prog.read_next(&[self.color() as i64]);
+            if let Some(color) = prog.run_to_out()? {
+                let turn = prog
+                    .run_to_out()?
+                    .ok_or("Program didn't return a direction")?;
+                self.step(Color::from(color), Turn::from(turn));
+            } else {
+                break;
+            }
+        }
+        Ok(())
+    }
 }
 
 fn part1(prog: &Intcode) -> Result<usize, String> {
     let mut robot = Robot::default();
-    let mut prog = prog.exec().read_vec(&[]).write_to(vec![]);
-    loop {
-        prog.read_next(&[robot.color() as i64]);
-        if let Some(color) = prog.run_to_out()? {
-            let turn = prog
-                .run_to_out()?
-                .ok_or_else(|| "Program didn't return a direction")?;
-            robot.step(Color::from(color), Turn::from(turn));
-        } else {
-            break;
-        }
-    }
+    robot.run(prog)?;
     Ok(robot.visited.len())
 }
 
-fn part2() -> u64 {
-    0
+fn part2(prog: &Intcode) -> Result<String, String> {
+    let mut robot = Robot::default();
+    robot.visited.insert((0, 0), White);
+    robot.run(prog)?;
+
+    let xmin = robot.visited.keys().min_by_key(|(x, _)| x).unwrap().0;
+    let xmax = robot.visited.keys().max_by_key(|(x, _)| x).unwrap().0;
+    let ymin = robot.visited.keys().min_by_key(|(_, y)| y).unwrap().1;
+    let ymax = robot.visited.keys().max_by_key(|(_, y)| y).unwrap().1;
+    let width = 1 + (xmax - xmin) as usize;
+    let height = 1 + (ymax - ymin) as usize;
+
+    let mut grid = [" "]
+        .repeat(width * height)
+        .chunks(width)
+        .map(|row| row.to_vec())
+        .collect::<Vec<Vec<_>>>();
+    for ((x, y), color) in robot.visited {
+        if color == White {
+            grid[(y - ymin) as usize][(x - xmin) as usize] = "\u{2588}";
+        }
+    }
+
+    grid.reverse();
+    Ok(grid
+        .iter()
+        .map(|row| row.join(""))
+        .collect::<Vec<_>>()
+        .join("\n"))
 }
 
 pub fn run() -> Result<String, String> {
     let input = include_str!("input/p11.txt");
     let prog = input.parse()?;
     let out1 = part1(&prog)?;
-    let out2 = part2();
-    Ok(format!("{} {}", out1, out2))
+    let out2 = part2(&prog)?;
+    Ok(format!("{}\n{}", out1, out2))
 }
 
 #[cfg(test)]
